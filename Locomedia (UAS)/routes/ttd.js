c4a1bc09c5fd9aa84edefa60954f9de561de50b6
Malepin <pramuditamelvin@gmail.com>
2021-11-16 20:58:14 +0700

    added more contents

--- Locomedia (UAS)/routes/ttd.js
+++ Locomedia (UAS)/routes/ttd.js
@@ -38,4 +38,32 @@ router.get('/yogyakarta', (req, res) => {
     res.render('pages/yogyakarta')
 })
 
+router.get('/sacredmonkeyforest', (req, res) => {
+    res.render('pages/sacredmonkeyforest')
+})
+
+router.get('/padarisland', (req, res) => {
+    res.render('pages/padarisland')
+})
+
+router.get('/lawangsewu', (req, res) => {
+    res.render('pages/lawangsewu')
+})
+
+router.get('/double6', (req, res) => {
+    res.render('pages/double6')
+})
+
+router.get('/merapi', (req, res) => {
+    res.render('pages/merapi')
+})
+
+router.get('/nusadua', (req, res) => {
+    res.render('pages/nusadua')
+})
+
+router.get('/kelingking', (req, res) => {
+    res.render('pages/kelingking')
+})
+
 module.exports = router;
