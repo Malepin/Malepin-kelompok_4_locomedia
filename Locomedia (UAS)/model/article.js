de46b9bb393ebf13af1b397582eeb8ea63c2c5d8
mjudianto <mathewjudianto0603@gmail.com>
2021-11-22 00:14:46 +0700

    add article to admin

--- Locomedia (UAS)/model/article.js
+++ Locomedia (UAS)/model/article.js
@@ -5,6 +5,10 @@ const articleSchema = mongoose.Schema({
         type: String,
         required: true
     },
+    link: {
+        type: String,
+        required: true
+    },
     sumber: {
         type: String,
         required: true
