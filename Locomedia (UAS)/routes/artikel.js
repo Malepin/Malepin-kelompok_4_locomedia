5273af31c26e2e27cdba746a893ea2802338fe5a
mjudianto <mathewjudianto0603@gmail.com>
2021-11-21 22:11:22 +0700

    article href

--- Locomedia (UAS)/routes/artikel.js
+++ Locomedia (UAS)/routes/artikel.js
@@ -6,4 +6,12 @@ router.get('/artikel1', (req, res) => {
     res.render('pages/artikel1')
 })
 
+router.get('/artikel2', (req, res) => {
+    res.render('pages/artikel1')
+})
+
+router.get('/artikel3', (req, res) => {
+    res.render('pages/artikel1')
+})
+
 module.exports = router;
